19d54458372638eecdbaa3c3ebeabd0d8d8e0cd2
bolishettianjali <bolishettianjali@gamil.com>
2021-12-27 13:12:18 +0530

    Practice problems

---
+++ objectentity.js
@@ -0,0 +1,6 @@
+let data={
+    name:'Jhon',age:25,mail:'Jhon@gmail.com'
+
+}
+console.log(Object.entries(data))
+console.log(Object.values(data));
---
+++ padstart.js
@@ -0,0 +1,3 @@
+let data="hello";
+console.log(data.padStart(6,'*'))
+console.log(data.padEnd(5,'*'))
